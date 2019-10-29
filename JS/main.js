14935fab5dd6172845422ce25e16b24cd22a465d
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-10-29 16:28:07 +0100

    Navbar and drop-down nav items added with bootstrap

--- JS/main.js
+++ JS/main.js
@@ -2,14 +2,14 @@ import house from '../data/pro-congress-115-house';
 import senate from '../data/pro-congress-115-senate';
 
 // Determin which data to use depending on location
-const determinDataToUse = () => {
+const toggleData = () => {
   const houseObj = {
     members: house.results[0].members,
-    table: document.getElementById("house-table")
+    table: document.querySelector(".house-table_tbody")
   }
   const senateObj = {
     members: senate.results[0].members,
-    table: document.getElementById("senate-table")
+    table: document.querySelector(".senate-table_tbody")
   }
 
   if (document.location.pathname === "/house-data.html") {
@@ -22,10 +22,12 @@ const determinDataToUse = () => {
 // Append appropriate data to table
 const appendData = fn => {
   const { members, table } = fn;
+
   members.forEach((mem, i) => {
     let tr = document.createElement("tr");
+
     tr.innerHTML =
-      `<td>${i + 1} 
+      `<td>${i + 1}. 
           ${mem.first_name} 
           ${mem.middle_name === null ? " " : mem.middle_name} 
           ${mem.last_name}
@@ -40,5 +42,5 @@ const appendData = fn => {
   });
 }
 
-determinDataToUse();
-appendData(determinDataToUse());
+toggleData();
+appendData(toggleData());
