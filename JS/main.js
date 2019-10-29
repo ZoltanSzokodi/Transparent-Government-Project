57116a6add3733ac377ce0331ecb8a28503bf5e8
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-10-29 10:44:35 +0100

    Raw Senate Table populated with local  data

--- JS/main.js
+++ JS/main.js
@@ -2,15 +2,35 @@ import axios from 'axios';
 import APIkey from '../data/APIkey';
 import senate from '../data/pro-congress-115-senate';
 
-
-
 // const url = "https://api.propublica.org/congress/v1/115/senate/members.json";
 // const config = {
 //   "X-API-Key": APIkey
 // }
-// const data = async axios.get(url, { headers: config }).then(response => {
+// const data = axios.get(url, { headers: config }).then(response => {
 //   console.log(response);
 // });
 
-// TESTING
-console.log(senate["results"][0]["members"][10]["id"]);
+const senateMembersArr = senate.results[0].members;
+const senateTable = document.getElementById("senate-table");
+
+console.log(senateMembersArr);
+
+
+const appendSenateData = data => {
+  data.forEach((senator, i) => {
+    let tr = document.createElement("tr");
+    tr.innerHTML =
+      `<td>${i + 1} ${senator.first_name} 
+          ${senator.middle_name === null ? " " : senator.middle_name} 
+          ${senator.last_name}
+      </td>
+      <td>${senator.party}</td>
+      <td>${senator.state}</td>
+      <td>${senator.seniority}</td>
+      <td>${senator.votes_with_party_pct}</td>`;
+    senateTable.appendChild(tr);
+  });
+}
+
+appendSenateData(senateMembersArr);
+
